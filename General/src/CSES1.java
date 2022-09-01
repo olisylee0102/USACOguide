a062f2683be1f7eaecf8452762d2e040adf5e14d
olisylee0102 <olisylee0102@gmail.com>
2022-08-31 21:02:39 -0700

    weird algorithm

---
+++ General/src/CSES1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class CSES1 {
+	
+	static Scanner in;
+	static long n;
+	
+	public static void main(String[] args) {
+		
+		in = new Scanner(System.in);
+		n = in.nextLong();
+		
+		System.out.print(n);
+		
+		while(n!=1) {
+			
+			if(n%2==0) n = n/2;
+			else n = n*3 + 1;
+			
+			System.out.print(" " + n);
+		}
+	}
+
+}
